92da6ee1978a3ddfebfd1fd4d90a156c2b6e1015
Tiffany DeVine <tiffany.devine@icloud.com>
2018-02-17 22:03:16 -0800

    Added test determine if App rendered child component CommentBox

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -3,12 +3,14 @@ import App from '../../src/components/app';
 
 // use describe to group similar tests
 describe('App', () => {
-  //use it to test a single attribute of a target (one specific test)
-  it('shows the correct test', () => {
-    //create instance of App
-    const component = renderComponent(App);
+  let component;
 
+  beforeEach(() => {
+    component = renderComponent(App);
+  });
+
+  it('shows a comment box', () => {
     //expect to make an assertion about a target
-    expect(component).to.contain('React simple starter');
+    expect(component.find('.comment-box')).to.exist;
   });
 });
