aabdf1403121c6352143079e5338b2131099df4e
Tiffany DeVine <tiffany.devine@icloud.com>
2018-02-17 21:39:27 -0800

    Created biolerpalte for CommentBox component

---
+++ src/components/CommentBox.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class CommentBox extends Component {
+  render() {
+    return (
+      <div className="box">
+        <textarea />
+        <button className="common-btn">Submit</button>
+      </div>
+    );
+  }
+}
+
+export default CommentBox;
