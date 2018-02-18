8377f75ee8a4fac1c389ae50541a04d39378d718
Tiffany DeVine <tiffany.devine@icloud.com>
2018-02-17 21:38:32 -0800

    Fixed error in comment box test

--- test/components/CommentBox_test.js
+++ test/components/CommentBox_test.js
@@ -1,19 +1,14 @@
 import { renderComponent, expect } from '../test_helper';
-import App from '../../src/components/CommentBox';
+import CommentBox from '../../src/components/CommentBox';
 
 describe('CommentBox', () => {
-  //use it to test a single attribute of a target (one specific test)
   it('has a text area', () => {
     const component = renderComponent(CommentBox);
-
-    expect(component.find('text-area')).to.exist;
+    expect(component.find('textarea')).to.exist;
   });
 
   it('has a button', () => {
-    //create instance of App
-    const component = renderComponent(App);
-
-    //expect to make an assertion about a target
-    expect;
+    const component = renderComponent(CommentBox);
+    expect(component.find('button')).to.exist;
   });
 });
