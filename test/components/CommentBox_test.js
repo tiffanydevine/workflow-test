eaea96014bc6688bb3dfcf2fbb76283cecf8e399
Tiffany DeVine <tiffany.devine@icloud.com>
2018-02-17 21:18:52 -0800

    Created CommentBox test file with test scaffold

---
+++ test/components/CommentBox_test.js
@@ -0,0 +1,14 @@
+import { renderComponent, expect } from '../test_helper';
+import App from '../../src/components/CommentBox';
+
+describe('CommentBox', () => {
+  //use it to test a single attribute of a target (one specific test)
+  it('', () => {
+    //create instance of App
+    const component = renderComponent(App);
+
+    //expect to make an assertion about a target
+    expect;
+  });
+});
+
