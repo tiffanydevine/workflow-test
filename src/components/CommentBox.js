991a20898351c0585f1f6ed1f1590fee2ece47a9
Tiffany DeVine <tiffany.devine@icloud.com>
2018-02-17 21:48:13 -0800

    changed highest level div className to comment-box

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class CommentBox extends Component {
   render() {
     return (
-      <div className="box">
+      <div className="comment-box">
         <textarea />
         <button className="common-btn">Submit</button>
       </div>
