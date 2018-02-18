f9e2cad4eb1fa4c78e10bb0a5901d65623337e07
Tiffany DeVine <tiffany.devine@icloud.com>
2018-02-17 22:30:51 -0800

    Added className to textarea for styling

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -14,7 +14,7 @@ class CommentBox extends Component {
   render() {
     return (
       <div className="comment-box">
-        <textarea value={this.state.comment} onChange={this.handleChange.bind(this)} />
+        <textarea className="styled-textarea" value={this.state.comment} onChange={this.handleChange.bind(this)} />
         <button className="common-btn">Submit</button>
       </div>
     );
